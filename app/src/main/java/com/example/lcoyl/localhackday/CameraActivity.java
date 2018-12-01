fc986256eb05678e95ea15042fb471079af260d1
James <james.ens@live.com>
2018-12-01 13:56:20 -0800

    Camera layout

--- app/src/main/java/com/example/lcoyl/localhackday/CameraActivity.java
+++ app/src/main/java/com/example/lcoyl/localhackday/CameraActivity.java
@@ -1,4 +1,28 @@
 package com.example.lcoyl.localhackday;
 
-public class CameraActivity {
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import static com.example.lcoyl.localhackday.MainActivity.getCameraInstance;
+
+public class CameraActivity extends Activity {
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
 }

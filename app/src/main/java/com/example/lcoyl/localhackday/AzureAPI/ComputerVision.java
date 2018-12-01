2c5aadeb28294062627c06968f35f2b273002101
Adam Collins <adamcollinssecure@gmail.com>
2018-12-01 12:48:37 -0800

    -Added CV package for Azure API. -Fixed gradle package dependencies for emulator.

---
+++ app/src/main/java/com/example/lcoyl/localhackday/AzureAPI/ComputerVision.java
@@ -0,0 +1,17 @@
+package com.example.lcoyl.localhackday.AzureAPI;
+
+import org.json.JSONObject;
+
+import java.io.File;
+
+public class ComputerVision {
+    private final String api_key = "699c3c58d8aa4194b3aa198dcb6c5ba7";
+    //Takes File object of an image and returns JSON object of data from API
+    public JSONObject imageData(File image){
+
+        return new JSONObject();
+    }
+
+
+
+}

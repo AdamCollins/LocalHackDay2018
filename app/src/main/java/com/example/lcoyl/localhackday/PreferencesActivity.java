c12769785ef6554a3dd83eb90f6636b6f5a00fd3
Naoreen <o7r1b@cs.ubc.ca>
2018-12-01 18:17:34 -0800

    Followed Youtube tutorial to hopefully get night mode stuff working???????

--- app/src/main/java/com/example/lcoyl/localhackday/PreferencesActivity.java
+++ app/src/main/java/com/example/lcoyl/localhackday/PreferencesActivity.java
@@ -1,5 +1,6 @@
 package com.example.lcoyl.localhackday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
@@ -8,6 +9,7 @@ import android.widget.Switch;
 
 public class PreferencesActivity extends AppCompatActivity {
 
+    private Switch nightModeSwitch;
     private Boolean isNightModeEnabled = true;
 
     @Override
@@ -16,12 +18,36 @@ public class PreferencesActivity extends AppCompatActivity {
                 ==AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.AppTheme_Dark);
         }
+        else setTheme(R.style.AppTheme_Light);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preferences_layout);
 
-        setupPowerButtonTakesPhotoSwitch();
-        setupDayModeSwitch();
+        nightModeSwitch = findViewById(R.id.nightModeSwitch);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            nightModeSwitch.setChecked(true);
+        }
 
+        nightModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    reset();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    reset();
+                }
+            }
+        });
+        //setupPowerButtonTakesPhotoSwitch();
+        //setupDayModeSwitch();
+
+    }
+
+    public void reset(){
+        Intent i = new Intent(getApplicationContext(), PreferencesActivity.class);
+        startActivity(i);
     }
 
 
@@ -41,7 +67,7 @@ public class PreferencesActivity extends AppCompatActivity {
     // EFFECTS: creates switch based on day mode switch in preferences_layout
     //          calls method to make app day mode if switch is checked
     private void setupDayModeSwitch() {
-        Switch dayModeSwitch = findViewById(R.id.dayModeSwitch);
+        Switch dayModeSwitch = findViewById(R.id.nightModeSwitch);
         dayModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {

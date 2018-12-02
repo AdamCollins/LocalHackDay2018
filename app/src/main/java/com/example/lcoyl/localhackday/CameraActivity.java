286191619a9d1912cb3083569a869c0d6c520d7b
James <james.ens@live.com>
2018-12-01 16:10:00 -0800

    fuck

--- app/src/main/java/com/example/lcoyl/localhackday/CameraActivity.java
+++ app/src/main/java/com/example/lcoyl/localhackday/CameraActivity.java
@@ -1,6 +1,8 @@
 package com.example.lcoyl.localhackday;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.widget.FrameLayout;
@@ -16,13 +18,25 @@ public class CameraActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photo_screen);
 
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
+
+        if(checkCameraHardware(this))
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
 
         // Create our Preview view and set it as the content of our activity.
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+    }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 
 }

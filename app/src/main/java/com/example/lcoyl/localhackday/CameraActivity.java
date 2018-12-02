61dc86f314f13682c2ab38bf72d2731143f9d0d1
James <james.ens@live.com>
2018-12-01 16:19:31 -0800

    My head hurts

--- app/src/main/java/com/example/lcoyl/localhackday/CameraActivity.java
+++ app/src/main/java/com/example/lcoyl/localhackday/CameraActivity.java
@@ -2,9 +2,16 @@ package com.example.lcoyl.localhackday;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import static com.example.lcoyl.localhackday.MainActivity.getCameraInstance;
@@ -19,14 +26,42 @@ public class CameraActivity extends Activity {
         setContentView(R.layout.photo_screen);
 
 
-        if(checkCameraHardware(this))
-            // Create an instance of Camera
-            mCamera = getCameraInstance();
+//        if(checkCameraHardware(this))
+//            // Create an instance of Camera
+//            mCamera = getCameraInstance();
+//
+//        // Create our Preview view and set it as the content of our activity.
+//        mPreview = new CameraPreview(this, mCamera);
+//        FrameLayout preview = findViewById(R.id.camera_preview);
+//        preview.addView(mPreview);
 
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        Button capture = findViewById(R.id.button_capture);
+        capture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            FrameLayout preview = findViewById(R.id.camera_preview);
+            Drawable d = new BitmapDrawable(getResources(), imageBitmap);
+            preview.setBackgroundDrawable(d);
+            //mImageView.setImageBitmap(imageBitmap);
+        }
     }
     /** Check if this device has a camera */
     private boolean checkCameraHardware(Context context) {
